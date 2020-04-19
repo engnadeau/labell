005639f75ad44822f753b6d4227dc8b256814e04
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2020-04-19 17:01:28 -0400

    hooked seo component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import CssBaseline from "@material-ui/core/CssBaseline";
+import "./index.css";
 import { ThemeProvider } from "@material-ui/core/styles";
-import theme from "./theme";
+import * as serviceWorker from "./serviceWorker";
+import App from "./App";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import SEO from "./SEO";
+import theme from "./theme";
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <React.StrictMode>
       <CssBaseline />
+      <SEO
+      title="Labell | Nicholas Nadeau"
+      image={process.env.PUBLIC_URL + '/logo512.png'} description="Enter text; get keywords, tags, and labels using natural language processing (NLP)"
+      url="https://labell.now.sh/"
+      />
       <App />
     </React.StrictMode>
   </ThemeProvider>,
