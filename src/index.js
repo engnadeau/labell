a6c533edf82f19ff88e649444b32bd15a6a396be
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2020-04-20 07:53:20 -0400

    removed uneeded

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import "./index.css";
 import { ThemeProvider } from "@material-ui/core/styles";
-import * as serviceWorker from "./serviceWorker";
 import App from "./App";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import React from "react";
@@ -23,8 +22,3 @@ ReactDOM.render(
   </ThemeProvider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
